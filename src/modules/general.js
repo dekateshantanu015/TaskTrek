8138be488879774d484ab4afedec09818626f300
Shantanu <dekateshantanu015@gmail.com>
2024-06-21 12:33:07 +0530

    modal form is now functioning

--- src/modules/general.js
+++ src/modules/general.js
@@ -1,27 +1,33 @@
 import { todos } from "./todos";
-import { createBtn, createDiv, createH2, createPara, createTodoCard } from "./create-dom-elements";
+import {
+  createBtn,
+  createDiv,
+  createH2,
+  createPara,
+  createTodoCard,
+} from "./create-dom-elements";
+import { openModal } from "./modal";
 
 const todoContent = () => {
-    const newTodo = todos[0];
-    return newTodo;
+  const newTodo = todos[0];
+  return newTodo;
 };
 
 const buildPage = () => {
-    const mainContainer = createDiv('main-container');
-    const title = createH2('project-title');
-    const desc = createPara('project-desc');
-    const todoContainer = createDiv('todo-container');
-    const btn = createBtn('todo-create');
+  const mainContainer = createDiv("main-container");
+  const title = createH2("project-title");
+  const desc = createPara("project-desc");
+  const todoContainer = createDiv("todo-container");
+  const btn = createBtn("todo-create");
 
-    title.innerText = "General";
-    desc.innerText = "A general list of random todos";
-    todoContainer.append(todoContent());
-    btn.innerText = "+";
+  title.innerText = "General";
+  desc.innerText = "A general list of random todos";
+  todoContainer.append(todoContent());
+  btn.innerText = "+";
+  btn.addEventListener("click", () => openModal());
 
-    mainContainer.append(title, desc, todoContainer, btn);
-    return mainContainer;
+  mainContainer.append(title, desc, todoContainer, btn);
+  return mainContainer;
 };
 
-export {
-    buildPage,
-}
+export { buildPage };
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,6 +1,24 @@
-const openModal = () => {
-  const modal = document.querySelector(".modal");
-  modal.classList.remove("display-none");
+import { createTodo } from "./todos";
+
+const modal = document.querySelector(".modal");
+const titleInput = document.querySelector(".modal-form-title-input");
+const dateInput = document.querySelector(".modal-form-date-input");
+
+const closeModal = document.querySelector(".modal-form-close");
+closeModal.addEventListener("click", () => modal.classList.add("display-none"));
+
+const modalForm = document.querySelector(".modal-form");
+modalForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createTodo(titleInput.value, dateInput.value);
+  clearModal();
+});
+
+const clearModal = () => {
+  titleInput.value = "";
+  dateInput.value = "";
 };
+
+const openModal = () => modal.classList.remove("display-none");
 
 export { openModal };
