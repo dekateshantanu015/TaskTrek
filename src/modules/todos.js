f1ff12c12804d26ea00982b8056e41c7f387030a
Shantanu <dekateshantanu015@gmail.com>
2024-06-21 12:19:48 +0530

    added factory function for creating new todo

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,8 +1,22 @@
-const todos = [{
-    type: 'general',
+const todos = [
+  {
+    type: "general",
     checked: false,
-    title: 'Todo-title',
-    date: 'dd-mm-yy',
-}];
+    title: "Todo-title",
+    date: "dd-mm-yy",
+  },
+];
 
-export { todos };
+const todoFactory = (title, date) => {
+  const type = "general";
+  const checked = true;
+  return { title, date, type, checked };
+};
+
+const createTodo = (title, date) => {
+  const newTodo = todoFactory(title, date);
+  todos.push(newTodo);
+  console.log(todos);
+};
+
+export { todos, createTodo };
