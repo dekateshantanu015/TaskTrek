31475824de101f6d16dc9391b62fa1e9eaf99a90
Shantanu <dekateshantanu015@gmail.com>
2024-06-25 15:12:46 +0530

    Fixed bug with Today and Upcoming pages

--- src/modules/build-pages.js
+++ src/modules/build-pages.js
@@ -36,6 +36,7 @@ const buildToday = () => {
   const desc = createPara("project-desc");
   const todoContainer = createDiv("todo-container");
 
+  mainContainer.setAttribute("data-id", "Today");
   title.innerText = "Today";
   desc.innerText = "All todos dated today";
 
@@ -52,6 +53,7 @@ const buildUpcoming = () => {
   const desc = createPara("project-desc");
   const todoContainer = createDiv("todo-container");
 
+  mainContainer.setAttribute("data-id", "Upcoming");
   title.innerText = "Upcoming";
   desc.innerText = "All upcoming todos in the next week";
 
